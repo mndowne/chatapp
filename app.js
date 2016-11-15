82203f5300fa7f57e0c0d551870a5e2dccf9fe01
mndowne <mndowne@gmail.com>
2016-11-15 01:33:12 -0600

    I'm at 14 minutes

--- app.js
+++ app.js
@@ -1,7 +1,25 @@
 var express = require('express');
 var app = express();
+var server = require('http').createServer(app);
+var io = require('socket.io').listen(server);
+var users = [];
+var connections = [];
+
+server.listen(process.env.PORT || 3000);
+
+console.log('server is up on 3000');
+
+app.get('/', function(req,res){
+  res.sendFile(__dirname + "/index.html");
+});
 
 
-app.listen(3000, function(){
-  console.log('server is up on 3000');
+io.sockets.on('connection',function(socket){
+  connections.push(socket);
+  console.log('Connected: %s sockets connected', connections.length);
+
+  //Disconnect
+  connections.splice(connections.indexOf(socket),1);
+  console.log('disconnected: %s sockets connected', connections.length);
+
 });
